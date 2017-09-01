a0596042f37d9ac65371bf594741b7cba1a0e193
James Adcock <jamesadcock1980@gmail.com>
2017-09-01 21:23:57 +0100

    Add project files.

---
+++ Stocky/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Stocky.MvcApplication" Language="C#" %>
---
+++ Stocky/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Stocky.Startup))]
+namespace Stocky
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
