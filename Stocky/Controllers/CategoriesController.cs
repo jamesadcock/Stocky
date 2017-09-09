7671ad67886f903c203244296d3c859dee64f68e
James Adcock <jamesadcock1980@gmail.com>
2017-09-09 14:35:24 +0100

    Adds edit category functionality

--- Stocky/Controllers/CategoriesController.cs
+++ Stocky/Controllers/CategoriesController.cs
@@ -25,6 +25,45 @@ namespace Stocky.Controllers
             _context.Dispose();
         }
 
+        public ActionResult New()
+        {
+            var categories = _context.Categories.ToList();
+  
+            return View("CategoryForm");
+        }
+
+
+        [HttpPost]
+        public ActionResult Save(Category category)
+        {
+            if (category.Id == 0)
+            {
+                _context.Categories.Add(category);
+            }
+            else
+            {
+                var categoryInDb = _context.Categories.Single(c => c.Id == category.Id);
+                categoryInDb.Name = category.Name;
+
+
+            }
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Categories");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var category = _context.Categories.SingleOrDefault(c => c.Id == id);
+
+            if (category == null)
+                return HttpNotFound();
+
+
+            return View("CategoryForm", category);
+
+        }
+
 
         public ViewResult Index()
         {
