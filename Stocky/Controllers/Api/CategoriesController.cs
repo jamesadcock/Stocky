3f74ae6a9192d5d70a999fe1264fde3cb4fd137e
James Adcock <jamesadcock1980@gmail.com>
2017-09-26 19:49:26 +0100

    Products category seletion complete

---
+++ Stocky/Controllers/Api/CategoriesController.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Stocky.Dtos;
+using Stocky.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Stocky.Controllers.Api
+{
+    public class CategoriesController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public CategoriesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+
+        public IEnumerable<CategoryDto> GetCategories()
+        {
+            var categoriesDto = _context.Categories.ToList().Select(Mapper.Map<Category, CategoryDto>);
+            return categoriesDto;
+        }
+    }
+}
